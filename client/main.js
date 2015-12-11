96dd22c9f5db7548ec625493e3d032b6e3a49d63
Jen Selby <jselby@nuevaschool.org>
2015-12-11 00:55:55 -0800

    Fix bizarre persistent messages
    - clear when user logs in
    - don't display if no user logged in

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ Accounts.ui.config({
 Accounts.onLogin(function () {
     document.cookie = 'simple-portfolio-server=' + Accounts._storedLoginToken() + '; path=/';
     Session.set('message', '');
-
+    Session.set('results', []);
 });
 
 Template.fileUpload.events({
